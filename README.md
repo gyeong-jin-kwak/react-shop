99f86f9a558c7be58e6504ed28184f5277a0323b
Sujin <kgj1212_@naver.com>
2020-12-21 21:53:31 +0900

    라우터 추가

--- README.md
+++ README.md
@@ -12,8 +12,18 @@
   2. css link 를 public/index.html 삽입
 * npm install url-loader --save-dev
 * npm run eject (to show webpack.config.js)
+* yarn add react-router-dom
 
 ## Use Bootstrap
 * div.row = 12 column
 * col-md-4 가운데 'md' 추가시 모바일 수직정렬
+
+## Router setting
+1. react-router-dom 설치
+2. index.js 에서 react-router-dom 의 BrowserRouter or HashRouter 선언
+3. 
+### BrowserRouter 과 HashRouter 차이
+* HashRouter 에는 # 기호, 서버로 전달이 안됨
+* BrowserRouter 서버로 전달됨, 서버 setting이 필요
+* Route 선언 <Route path="/" component={} >
 
--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import Product from './components/Product';
 import productData from './data';
 
+import { Link, Route, Switch } from 'react-router-dom';
+
 function App() {
   let [products, setProducts] = useState(productData);
 
@@ -27,34 +29,52 @@ function App() {
         </Navbar.Collapse>
       </Navbar>
 
-      <Jumbotron className="main-copy">
-        <h1>Welcome, Inchant Field!</h1>
-        <p>
-          여행,액티비티,일상생활에도 Inchant Field 와 함께라면 어디든 갈 수 있어!
-          착하고 정직하게 만든, 매일 입고 싶은 옷
-        </p>
-        <p>
-          <Button variant="primary">더보기</Button>
-        </p>
-      </Jumbotron>
+      <Route path="/" exact>
+        <Jumbotron className="main-copy">
+          <h1>Welcome, Inchant Field!</h1>
+          <p>
+            여행,액티비티,일상생활에도 Inchant Field 와 함께라면 어디든 갈 수 있어!
+            착하고 정직하게 만든, 매일 입고 싶은 옷
+          </p>
+          <p>
+            <Button variant="primary">더보기</Button>
+          </p>
+        </Jumbotron>
 
-      <div className="container">
-        <div className="row">
-          {
-            products.map((product)=>{
-              return(
-                <Product 
-                  key={product.id} 
-                  title={product.title} 
-                  price={product.price} 
-                  content={product.content}
-                  url={product.url} 
-                />
-              )
-            })
-          }
+        <div className="container">
+          <div className="row">
+            {
+              products.map((product)=>{
+                return(
+                  <Product 
+                    key={product.id} 
+                    title={product.title} 
+                    price={product.price} 
+                    content={product.content}
+                    url={product.url} 
+                  />
+                )
+              })
+            }
+          </div>
         </div>
-      </div>
+      </Route>
+      <Route path="/detail">
+        <div className="containter">
+            <div className="row">
+              <div className="col-md-6">
+                <img src="./images/cap_black.gif" alt="검은색 모자" width="100%" />
+              </div>
+              <div className="col-md-6 mt-4">
+                <strong className="pt-5">상품명</strong>
+                <p>상품설명</p>
+                <span>12000</span>
+                <button className="btn btn-danger">주문하기</button>
+              </div>
+            </div>
+        </div>
+      </Route>
+
     </div>
   );
 }
