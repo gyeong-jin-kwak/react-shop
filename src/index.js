3fe24be4fceb8255a48fb4280bad4edb353fb16a
Sujin <kgj1212_@naver.com>
2020-12-28 11:59:52 +0900

    build

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="react-shop">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
