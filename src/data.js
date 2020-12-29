ee44864d390822f8afa49ededb7e3e62aa0428cc
Sujin <kgj1212_@naver.com>
2020-12-29 11:57:19 +0900

    fix: 이미지 경로 수정

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export default [
     title: "캡모자 black",
     price: 25000,
     content: "골프 캡모자",
-    url: "./images/cap_black.gif",
+    url: "https://postfiles.pstatic.net/MjAyMDEyMjlfNjYg/MDAxNjA5MjEwNTI1MjM4.BEE8VZwWbjzJ9cfAe2MVXJ7dVnbOOAEdJ1bnqB9Z33Ig.egxvqaQw2Uth2xzh91wwXubRr3VJ6ic6JtbPvIMwLDIg.GIF.kgj1352/cap_black.gif?type=w773",
     num: 10
   },
   {
@@ -12,7 +12,7 @@ export default [
     title: "캡모자 beige",
     price: 25000,
     content: "골프 캡모자",
-    url: "./images/cap_beige.gif",
+    url: "https://postfiles.pstatic.net/MjAyMDEyMjlfMTI5/MDAxNjA5MjEwNTI1MjM1.IfY1wDFnFYowtUR5pdBve1Ryn72_qzp2do3dIwVR-z4g.UEKsb94ZbGLoy74Wj_xsQmUtQh7lJpi-DVv7N73NhFcg.GIF.kgj1352/cap_beige.gif?type=w773",
     num: 7
   },
   {
@@ -20,7 +20,7 @@ export default [
     title: "top",
     price: 38000,
     content: "outdoor",
-    url: "./images/top.gif",
+    url: "https://postfiles.pstatic.net/MjAyMDEyMjlfMTM3/MDAxNjA5MjEwNTI1MTkw._uyU2XK1fdXchlxvw41y8P3NssWrAzMgVj-p8hIkgLYg.E2o5dG6D15GQPuy5KxAY7swhCAKEhHEZzrw0DnO_XBkg.GIF.kgj1352/top.gif?type=w773",
     num: 21
   }
 ]
